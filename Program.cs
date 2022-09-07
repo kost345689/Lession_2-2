96487a95119a585f1395514585b740642ca88352
kostya <kostya_pgs@mail>
2022-09-07 12:50:23 +0700

    4

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+        
+        Console.WriteLine("Введите числа");
+
+        int number = Convert.ToInt32(Console.ReadLine());
+         
+         string numberText = Convert.ToString(number);
+
+            if(numberText.Length > 2)
+         
+               {
+
+                Console.WriteLine("третье число - >" + numberText[2]);
+
+               }
+
+            else 
+              
+              {
+
+               Console.WriteLine("третье число нет");
+
+
+              }
+
+
+
+
+
+       
